65f14107fe3e75950e009c3a389a8e3fd64bea71
michael snyder <mike.p.snyder@gmail.com>
2026-01-29 11:48:27 -0800

    adding micro  ecc

--- external/segger_rtt/SEGGER_RTT_Syscalls_SES.c
+++ external/segger_rtt/SEGGER_RTT_Syscalls_SES.c
@@ -73,7 +73,7 @@ Revision: $Rev: 4351 $
 #include <stdio.h>
 #include "limits.h"
 #include "__libc.h"
-#include "__vfprintf.h"
+//#include "__vfprintf.h"
 
 /*********************************************************************
 *
@@ -242,8 +242,8 @@ int puts(const char *s) {
 *  Function description
 *    Write one character via RTT.
 */
-int __putchar(int x, __printf_tag_ptr ctx) {
-  (void)ctx;
+int __putchar(int x, /*__printf_tag_ptr ctx*/void *arg) {
+//  (void)ctx;
   SEGGER_RTT_Write(0, (char *)&x, 1);
   return x;
 }
